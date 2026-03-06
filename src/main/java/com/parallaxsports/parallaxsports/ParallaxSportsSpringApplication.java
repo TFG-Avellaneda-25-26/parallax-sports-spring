9889a3c74231167e5a59e15fa8de778d3a502544
diegokoes <stopwatch1567@proton.me>
2026-03-06 11:15:32 +0100

    project initial set up

---
+++ src/main/java/com/parallaxsports/parallaxsports/ParallaxSportsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.parallaxsports.parallaxsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallaxSportsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParallaxSportsSpringApplication.class, args);
+    }
+
+}
