3fea914d7799b431e0018276a212e22bdd8026d7
Ignacio Cambronero Mariño <ignacio.cambronero@educa.madrid.org>
2026-05-07 19:04:55 +0200

    Borrar archivos
    Archivos innecesarios borrados

--- src/main/java/dev/parallaxsports/ParallaxSportsApiApplication.java
+++ src/main/java/dev/parallaxsports/ParallaxSportsApiApplication.java
@@ -2,12 +2,10 @@ package dev.parallaxsports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaRepositories(basePackages = {"dev.parallaxsports"})
 public class ParallaxSportsApiApplication {
 
     public static void main(String[] args) {
